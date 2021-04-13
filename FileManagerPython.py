efc26822506644749123e27d85dded64bc36c34d
4lfman <31034983+4lfman@users.noreply.github.com>
2021-04-13 18:10:51 +0200

    Changed how the files are organized

--- FileManagerPython.py
+++ FileManagerPython.py
@@ -9,56 +9,71 @@ import time
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
         for filename in os.listdir(folder_to_track):
-            src = folder_to_track + "/" + filename
+            HandleFile(filename)
 
-            sub_folder = time.ctime(os.path.getctime(src)) #Gets the time in seconds since 1970 and converts to string
-            sub_folder = time.strptime(sub_folder) #Gets the time from the string
-            sub_folder = time.strftime("%Y-%m", sub_folder) #Formats the folders with year and month of the file creation
+def HandleFile(filename):
 
-            new_destination = folder_destination + "/" + sub_folder
+    #The filetypes
+    #region
+    def folder():
+        print(filename + ": Was a folder")
+        return 'Folder'
+    
+    def picture():
+        print(filename + ": Was a picture")
+        return 'Picture'
 
-            if (os.path.isdir(src)):
-                print("Folder")
-                if not (os.path.isdir(new_destination)): #If the directory does not exist, create it
-                    os.mkdir(new_destination)
-                os.rename(src, new_destination + "/" + filename) #Move the file/folder to the new directory
-            else:
-                HandleFile(src, new_destination, filename)
-
-def HandleFile(src, new_destination, filename):
     def txt():
         print(filename + ":Was .txt")
         return 'txt'
-
-    def picture():
-        print(filename + ": Was a picture")
-        os.rename(src, "C:/Users/Alfred/Desktop/ToSort/" + filename)
-        return None
 
     def zip():
         print(filename + ": Was a zip file")
         return 'zip'
 
+    def lnk():
+        print(filname + ': Was a shortcut(.lnk)')
+        return 'Shortcut'
+
+    def app():
+        print(filename + ': Was an app')
+        return 'App'
+
     def notAvalible():
         print("{} filetype is not avalible for now.".format(extention))
         return 'unavalible'
+    
+    #endregion
 
+    src = folder_to_track + "/" + filename
+    
     extention = os.path.splitext(filename)[1]
     if extention in ['.png', '.jpeg', '.jpg', '.gif']: #Lumps all picture formats into one method
         extention = 'picture'
+    elif os.path.isdir(src): #Checks if the input is a folder
+        extention = 'Folder'
 
     dict = {
+        'Folder' : folder,
+        'picture' : picture,
         '.txt' : txt,
         '.log' : txt,
-        'picture' : picture,
-        '.zip' : zip
+        '.zip' : zip,
+        '.lnk' : lnk,
+        '.exe' : app
         }
     filetype = dict.get(extention, notAvalible)() #Assigns the return of the method from the dict to 'filetype'
 
+    time_folder = time.ctime(os.path.getctime(src)) #Gets the time in seconds since 1970 and converts to string
+    time_folder = time.strptime(time_folder) #Gets the time from the string
+    time_folder = time.strftime("%Y-%m", time_folder) #Formats the folders with year and month of the file creation
+
+    new_destination = (folder_destination + '/' + filetype + '/' + time_folder)
+
     if filetype != None:
-        if not (os.path.isdir(new_destination + '/' + filetype)): #If the directory does not exist, create it
-            os.makedirs(new_destination + '/' + filetype)
-        os.rename(src, new_destination + '/' + filetype + '/' + filename) #Move the file/folder to the new directory
+        if not (os.path.isdir(new_destination)): #If the directory does not exist, create it
+            os.makedirs(new_destination)
+        os.rename(src, (new_destination + '/' + filename)) #Move the file/folder to the new directory
 
 folder_to_track = "/Users/Alfred/Desktop/AutoSort"
 folder_destination = "/Users/Alfred/Desktop/SortByFileType"
